ee4bd9d625caf03fcd955ddde72f9a7b03c91adf
habilithm97 <habinkim97@gmail.com>
2022-01-29 00:57:22 +0900

    위치 공부 및 실습

--- app/src/main/java/com/example/locationmaster/MainActivity.java
+++ app/src/main/java/com/example/locationmaster/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.locationmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.zip.DeflaterOutputStream;
 
 /*
 *위치 관리자(LocationManager) : 시스템 서비스가 관리함
@@ -24,7 +29,7 @@ import org.w3c.dom.Text;
 -4. 매니페스트에 권한 추가 : GPS를 사용할 수 있도록 매니페스트 파일에 ACCESS_FINE_LOCATION 권한을 추가하고 위험권한을 위한 설정과 코드를 추가함
 */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     TextView tv;
 
@@ -44,19 +49,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void MyLocationService() {
-        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); // 위치 관리자 객체 참조
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); // 1. 위치 관리자 객체 참조
 
         try {
             Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER); // getLastKnownLocation() : 최근 위치 정보 확인, GPS_provider : 위치 제공자
             if (location != null) {
                 double latitude = location.getLatitude();
                 double longitude = location.getLongitude();
-                String msg = "최근 나의 위치 : 위도 : " + latitude + "\n경도 : " + longitude;
+                String msg = "최근 나의 위치 -> 위도 : " + latitude + "\n경도 : " + longitude;
 
                 tv.setText(msg);
             }
+
+            // 3. 위치 정보 업데이터 요청
+            GPSListener gpsListener = new GPSListener(); // 리스너 객체 생성
+            // 10초마다 내 위치 정보를 전달받게됨
+            long minTime = 10000; // 최소 시간 10초
+            float minDistance = 0; // 최소 거리 0
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener); // 위치 요청
+            Toast.makeText(getApplicationContext(), "내 위치 확인 요청함. ", Toast.LENGTH_SHORT).show();
+
         } catch (SecurityException e) {
             e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener { // 2. 위치 리스너 구현
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) { // 위치가 확인 되었을 때 그 정보를 받아서 처리
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+            String msg = "내 위치 -> 위도 : " + latitude + "\n경도 : " + longitude;
+            tv.setText(msg);
         }
     }
 }
